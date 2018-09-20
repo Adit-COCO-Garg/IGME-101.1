7bca0ea24dd61c822e91afbaf7318d2fa0d213e7
Adit-COCO-Garg <adit.garg21k@gmail.com>
2018-09-19 21:05:50 -0400

    Week:04 Day:2 Commit:3

--- Homework/HW1 dice roll/sketch.js
+++ Homework/HW1 dice roll/sketch.js
@@ -5,17 +5,17 @@
  */
 
 "use strict"; //catch some common coding errors
-var exclMark, diceButton, rollDice, ellipPositionX, rollDiceValue;
+var exclMark, diceButton, rollDice, rollDiceValue, dotXposition;
 /**
  * setup : Initialization runs once; called automatically
  * The objectve of this program is allow the user to "roll a dice" by clicking the green button.
  */
 function setup() {
-	createCanvas(400, 400);
-	colorMode(HSB, 360, 100, 100);
-	background(195, 2, 83);
+	createCanvas(400, 400); //Canvas intialized
+	colorMode(HSB, 360, 100, 100); //Max hue, max saturation, and max brightness. And color mode set to HSB
+	background(195, 2, 83); //Light gray background
 	noStroke();
-	exclMark();
+	exclMark(); //Calls the exclamation mark in function as it would only be drawn once
 }
 
 /**
@@ -23,50 +23,54 @@ function setup() {
  * Summarize code that you add
  */
 function draw() {
-	diceButton();
+	diceButton(); //This function draws the button
 }
 
 function exclMark() {
 	fill("black");
-	rect(180, 70, 40, 100);
-	ellipse(200,200,40,40);
-	
-
+	rect(180, 70, 40, 100); 
+	ellipse(200, 200, 40, 40);// The exclamation mark is horizontally and vertically centered on the screen.
 }
 
-function diceButton() {
+function diceButton() { //The function to draw the dice button
 	fill(120, 100, 50);
 	rect(270, 20, 100, 50);
 }
 
 function mousePressed() {
-	if ((mouseX >= 270) && (mouseY >= 20) && (mouseX <= 370) && (mouseY <= 70)) {
-		background(195, 2, 83);
-		rollDice();
+	if ((mouseX >= 270) && (mouseY >= 20) && (mouseX <= 370) && (mouseY <= 70)) { //Bounding conditions for button created, allowing the "dice to be rolled only when the user clicks inside the "dark green button"
+		background(195, 2, 83);// Previous roll/ instance cleared
+		rollDice(); //This function rolls the dice
 	}
 }
 
 function rollDice() {
-	rollDiceValue = int(random(1, 6));
-	print(rollDiceValue);
-	fill((random(0, 255)), 100, 100);
-	if (rollDiceValue >= 1) {
-		ellipse(40, 250, 40, 40);
+	dotXposition=40; //Intialize position variable for 'X' position of the dots.
+	rollDiceValue = int(random(1, 6)); //Randomly generates a value between 1 to 6 (inclusive). Which is similar to rolling the dice.
+	print(rollDiceValue);// Prints the value generated in console.
+	fill((random(0, 255)), 100, 100); //Assigns the dots a random hue value between 0 and 255 (inclusive) and ensures each dot has max saturation and brinhtness
+	if (rollDiceValue >= 1) {//each if block draws the dot once, depending on the value of the roll.
+		ellipse(dotXposition, 250, 40, 40);
 	}
 	if (rollDiceValue >= 2) {
-		ellipse(100, 250, 40, 40);
+		dotXposition+=60;
+		ellipse(dotXposition, 250, 40, 40);
 	}
 	if (rollDiceValue >= 3) {
-		ellipse(160, 250, 40, 40);
+		dotXposition+=60;
+		ellipse(dotXposition, 250, 40, 40);
 	}
 	if (rollDiceValue >= 4) {
-		ellipse(220, 250, 40, 40);
+		dotXposition+=60;
+		ellipse(dotXposition, 250, 40, 40);
 	}
 	if (rollDiceValue >= 5) {
-		ellipse(280, 250, 40, 40);
+		dotXposition+=60;
+		ellipse(dotXposition, 250, 40, 40);
 	}
 	if (rollDiceValue >= 6) {
-		ellipse(340, 250, 40, 40);
+		dotXposition+=60;
+		ellipse(dotXposition, 250, 40, 40);
 	}
 
 }
