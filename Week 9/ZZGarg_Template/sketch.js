6fde15b2b6825594e2c4126cc8d6ab0fa0af3709
Adit Garg (RIT Student) <adit.garg21k@gmail.com>
2018-10-26 09:54:40 -0400

    Week: 09 Day: 03 Commit: 1

---
+++ Week 9/ZZGarg_Template/sketch.js
@@ -0,0 +1,30 @@
+/**
+ * Adit Garg
+ * IGME-101: Assignment Name, m/d/18
+ * Summarization of sketch activity
+ */
+
+"use strict"; //catch some common coding errors
+
+var oak1;
+/**
+ * setup : Initialization runs once; called automatically
+ * Summarize code that you add
+ */
+function setup() {
+	createCanvas(1000,1000);
+	print(oak1);
+	//Oask1's undefined
+	oak1 = new Tree();
+	print(oak1);
+	//Oak1's now defined as an object of class tree
+
+}
+
+/**
+ * draw : Periodically called automatically
+ * Summarize code that you add
+ */
+function draw() {
+
+}
